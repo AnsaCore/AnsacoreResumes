abb1af38c7f470ed3a19fa8b2ce3055d24c1e4e6
Sam Mylle <mylle.sam@gmail.com>
2021-12-05 20:07:12 +0100

    API integration

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,26 @@ AOS.init({
 });
 
 // Add your javascript here
+function submit() {
+    var data = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    }
+
+    $.ajax(
+        {
+            url: "https://api.ansacore.com",
+            body: JSON.stringify(data),
+            type: "POST",
+            success: function(result) {
+                console.log('yay');
+            }
+        },
+        {
+            error: function(error) {
+                console.log('error');
+            }
+        }
+    )
+}
